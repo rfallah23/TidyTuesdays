7e584d149c27702f902ddaf2962c72ce95491bac
Roshan <roshan.fallah.621@my.csun.edu>
2023-02-12 08:02:28 -0800

    few changes

--- week_03/script/TidyT_no1_week3.R
+++ week_03/script/TidyT_no1_week3.R
@@ -11,23 +11,26 @@ library(here)
 glimpse(penguins)
 
 ggplot(data=penguins,
-       mapping = aes(x = bill_length_mm,
-                     y = body_mass_g,
+       mapping = aes(x = bill_depth_mm,
+                     y = bill_length_mm,
                      group = species,
                      color = species))+
   geom_boxplot()+
-  geom_smooth(aes(group = "lm"))+
+  geom_jitter()+
   labs(x = "Bill Length (mm)",
        y = "Body Mass (g)"
       
   )+
 scale_color_manual(values = beyonce_palette(11))+
-  scale_x_continuous(breaks = c(40,46,52),
-  labels = c("low", "medium", "high"))+
+  #scale_x_continuous(breaks = c(40,46,52),
+  #labels = c("low", "medium", "high"))+
 theme_bw()+
   theme(axis.title = element_text(size = 16,
                                   color = "purple"),
-  panel.background = element_rect(fill = "pink"))
+  panel.background = element_rect(fill = "pink"))+
+  ggtitle("The relationship between weight and bill length in 3 penguins species")+
+  theme(plot.title = element_text(color = "red"))
+
   
 ggsave(here("week_03","output","TT1_penguin.png"))
 
